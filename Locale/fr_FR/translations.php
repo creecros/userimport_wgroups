c60c4929223284d0b5b34a993eaf8a0537d9accb
Craig Crosby <creecros@gmail.com>
2018-12-17 11:21:44 -0500

    Create translations.php

---
+++ Locale/fr_FR/translations.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'ImportWithGroup'                             => 'ImporterAvecUnGroupe',
+    'Import Users with Group'                     => 'Importer des utilisateurs avec un groupe',
+];
